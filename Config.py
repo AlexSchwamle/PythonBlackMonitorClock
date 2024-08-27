c0c7d77e84e8ba5accf478049d634b44ef7847e2
AlexSchwamle <alschwamle@gmail.com>
2024-08-26 23:04:31 -0600

    Change config names to monitor # and CONST_UNDERSCORE format

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 # Add the third `, True` to any monitor to show the clock on that monitor. You can display the clock on none or all 3 - it's modular! 
-left_screen_middle = -1437, 272
-top_screen_middle = 272, 248, True 
-right_screen_middle = 2179, 198 
+MONITOR_1_POS = -1461, 561
+MONITOR_2_POS = 437, 237, True 
+MONITOR_3_POS = 2742, 506
 
 # Make sure your browser's exe is in your path or you can specify the full path
-cmdToOpenBrowser = "start brave.exe" 
+CMD_TO_OPEN_BROWSER = "start brave.exe" 
